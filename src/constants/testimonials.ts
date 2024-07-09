9b67746a113fb42d7f7c8f75faca0243b5b6ccce
Diego Reyes Cabrera <diegoreyes150103@gmail.com>
2024-07-08 21:20:30 -0400

    feat: add testimonials section

---
+++ src/constants/testimonials.ts
@@ -0,0 +1,52 @@
+export type Testimonial = {
+  name: string;
+  website: string;
+  description: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+};
+
+export const TESTIMONIALS = [
+  {
+    name: "John Doe",
+    website: "https://www.google.com",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
+    image: {
+      src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "John Doe",
+    },
+  },
+  {
+    name: "Jane Doe",
+    website: "https://www.google.com",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
+    image: {
+      src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "Jane Doe",
+    },
+  },
+  {
+    name: "John Doe",
+    website: "https://www.google.com",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
+    image: {
+      src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "John Doe",
+    },
+  },
+  {
+    name: "Jane Doe",
+    website: "https://www.google.com",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
+    image: {
+      src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "Jane Doe",
+    },
+  },
+] as const satisfies Testimonial[];
